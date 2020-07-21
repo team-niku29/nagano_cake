91cb8f3590f32c5005402d9cf9b9afc5aec6b39e
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-07-21 17:18:23 +0900

    [Add] Order_index_tentative

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,8 +4,16 @@ class Admin::OrdersController < ApplicationController
   end
 
   def index
+    case params[:order_sort]
+    when "0"
+      @orders = Order.where(created_at: Date.today)
+    when "1"
+      @user = ClientUser.find(params[:id]) #idの取得方法はあとで考えます Shoki
+      @orders = @user.orders
+    else
+      @orders = Order.all
+    end
     @orders = Order.all
-
   end
   
   def show
