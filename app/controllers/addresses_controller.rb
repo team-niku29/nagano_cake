0e395d24039de7bea7d9f11e480144ee68665739
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-07-21 21:20:45 +0900

    [repair]test pull request

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,6 +3,13 @@ class AddressesController < ApplicationController
   end
 
   def create
+    @address = Address.new(address_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @address.client_user_id = current_user.id
+    if @address.save #入力されたデータをdbに保存する。
+      redirect_to request.referer, notice: "successfully created address!"#保存された場合の移動先を指定。
+    else
+      redirect_to request.referer
+    end
   end
 
   def edit
@@ -10,7 +17,14 @@ class AddressesController < ApplicationController
 
   def update
   end
-  
+
   def destroy
   end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address ,:name)
+  end
+
 end
