bbfd846c4bab5be768b89b2a88b4f7c390bdff17
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-07-21 11:47:52 +0900

    FirstCommit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,16 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
   
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_products_index_path
   end
 
   def index
+    @products = Product.all
   end
 
   def show
@@ -14,6 +19,11 @@ class Admin::ProductsController < ApplicationController
   def edit
   end
   def update
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :genre_id, :price_excluding, :out_of_stock, :image_id)
   end
 
 end
