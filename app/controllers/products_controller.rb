33edc8f13f6bc72ab22248fdfc4c4e1bcf3a2aca
nizutaro <niduka81@gmail.com>
2020-07-21 11:18:08 +0900

    [add]devise情報の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,27 @@
 class ProductsController < ApplicationController
+
   def index
   end
 
   def show
+  	@cart = Cart.new
+    @product = Product.find(params[:id])
+  	#if user_signed_in?
+  		#@client_user = current_user
+  	#else
+  	#end
   end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to request.referer
+  end
+
+  private
+
+  def product_params
+  	params.require(:product).permit(:name,:introduction,:price_excluding)
+  end
+
 end
