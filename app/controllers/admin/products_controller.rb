80292f247ea78d838cbc6ba852cca0ed219f9ecc
baDo0111 <cacf76afd78nyr@gmail.com>
2020-07-23 18:40:01 +0900

    save

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,8 @@
 class Admin::ProductsController < ApplicationController
   def new
-
     @product = Product.new
   end
-  
+
   def create
     @product = Product.new(product_params)
     @product.save
@@ -15,13 +14,20 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
+
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admin_product_path(@product.id)
   end
-  
+
   private
   def product_params
     params.require(:product).permit(:name, :introduction, :genre_id, :price_excluding, :out_of_stock, :image_id)
