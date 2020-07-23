ea5a44139c133080b9e87166fa61b88c764ffdb4
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-07-23 16:34:58 +0900

    [Add] 会員編集画面作成(デザイン抜き)OK

--- app/controllers/admin/client_users_controller.rb
+++ app/controllers/admin/client_users_controller.rb
@@ -4,12 +4,20 @@ class Admin::ClientUsersController < ApplicationController
 
   def show
     @user = ClientUser.find(params[:id])
-    binding.pry
   end
 
   def edit
+    @user = ClientUser.find(params[:id])
   end
   
   def update
+    @user = ClientUser.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_client_user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:client_user).permit(:family_name, :first_name, :kana_family_name, :kana_first_name, :postal_code, :address, :tel, :email, :delete_status)
   end
 end
