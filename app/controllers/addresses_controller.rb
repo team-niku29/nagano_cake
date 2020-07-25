2ace2ba2e9c615a91e671174da2dd33942bc7a8f
nizutaro <niduka81@gmail.com>
2020-07-25 10:43:40 +0900

    [add]addressの追加

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,18 +2,17 @@ class AddressesController < ApplicationController
   def index
     @address = Address.new
     @client_user = current_client_user
-        
-
+    @addresses = @client_user.addresses.reverse_order
   end
 
   def create
-    @address = Address.new(address_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-    @address.client_user_id = current_user.id
+    @address = Address.new(address_params) #addressモデルのテーブルを使用しているのでaddressコントローラで保存する。
+    @address.client_user_id = current_client_user.id
     if @address.save #入力されたデータをdbに保存する。
       redirect_to request.referer, notice: "successfully created address!"#保存された場合の移動先を指定。
     else
       redirect_to request.referer
-      binding.pry
+      
       
     end
   end
