4fc3fe1e6dd1022b2afb7959bd36861293d6ae51
nizuka <55015069+nizutaro@users.noreply.github.com>
2020-07-25 11:13:33 +0900

    Update addresses_controller.rb

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,11 +6,8 @@ class AddressesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    @address = Address.new(address_params)
-=======
+
     @address = Address.new(address_params) #addressモデルのテーブルを使用しているのでaddressコントローラで保存する。
->>>>>>> 5e00ac1d6bd0c46bf4b6681079ead4cf962aac55
     @address.client_user_id = current_client_user.id
     if @address.save #入力されたデータをdbに保存する。
       redirect_to request.referer, notice: "successfully created address!"#保存された場合の移動先を指定。
