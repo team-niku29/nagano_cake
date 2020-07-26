f3b5e3e478ea9b133f6bb76e407d6c3672464d9b
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-07-26 22:46:40 +0900

    [Fix] 管理者側トップ画面の当日注文件数取得メソッド作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,7 @@ class Order < ApplicationRecord
 
   enum status_flg: { "入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4}
   enum payment_flg: { "銀行振込": 0, "クレジットカード": 1}
+
+  scope :today_orders, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
+
 end
