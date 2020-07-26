35666592833de7a3066682a15400dde40c2d2089
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-07-26 15:03:18 +0900

    [add]producct_index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,23 @@
 class ProductsController < ApplicationController
 
   def index
+    #退会ユーザーをアクセスさせない
     if client_user_signed_in?
-      if current_client_user.delete_status == false  
+      if current_client_user.delete_status == false
         redirect_to destroy_client_user_session_path
       end
     end
+
+    #商品一覧表示しない
+    @TAX = 1.08
+    @products = Product.page(params[:page]).reverse_order
+    @number = 0
+
+    @products.each do |product|
+      @number = @number + 1
+    end
   end
+
   def show
     @TAX = 1.08
     @product = Product.find(params[:id])
