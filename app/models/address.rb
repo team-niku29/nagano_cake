e8f66e391f00564e605abc1fd9fea0353f14639f
nizutaro <niduka81@gmail.com>
2020-07-27 09:49:28 +0900

    [add]addressのeditビュー追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :client_user
-  # validates address: 
   def full_address
     self.postal_code + self.address + self.name
   end
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :name, presence: true
 
 end
--- app/models/client_user.rb
+++ app/models/client_user.rb
@@ -6,4 +6,15 @@ class ClientUser < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :carts, dependent: :destroy
+
+  validates :kana_first_name, presence: true,
+                 format: {
+                   with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+                   message: "全角カタカナのみで入力して下さい"
+                 }
+  validates :kana_family_name, presence: true,
+  format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+}
 end
